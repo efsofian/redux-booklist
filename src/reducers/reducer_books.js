fa0144963a335d6e6040ecfc7c128483ca80e659
efsofian <ef.sofian@gmail.com>
2018-11-13 09:19:08 +0100

    reduxen

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return(
+    [
+      { title: 'La Main Noir', pages: 104 },
+      { title: 'Dersou Ouzala', pages: 205 },
+      { title: 'Le diner de con', pages: 350 },
+    ]
+  );
+}
